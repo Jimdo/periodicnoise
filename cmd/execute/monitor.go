c2850ec9cd5902cec546f1659673bfe52cd227b6
Ingo Oeser <ingo@jimdo.com>
2013-04-17 14:05:27 +0200

    abstract away actual exec implementation to faciliate testing

--- cmd/execute/monitor.go
+++ cmd/execute/monitor.go
@@ -6,24 +6,24 @@ import (
 	"strings"
 )
 
-var monitoringCalls map[monitoringResult] string
+var monitoringCalls map[monitoringResult]string
 var monitoringEvent string
 
 type monitoringResult string
 
 const (
-	monitorOk monitoringResult = "OK"
+	monitorOk       monitoringResult = "OK"
 	monitorCritical monitoringResult = "CRITICAL"
-	monitorWarning monitoringResult = "WARNING"
-	monitorDebug monitoringResult = "DEBUG"
-	monitorUnknown monitoringResult = "UNKNOWN"
+	monitorWarning  monitoringResult = "WARNING"
+	monitorDebug    monitoringResult = "DEBUG"
+	monitorUnknown  monitoringResult = "UNKNOWN"
 )
 
 // Hook for passive monitoring solution
 func monitor(state monitoringResult, message string) {
 	// sanity check arguments
 	switch state {
-	case monitorOk,monitorCritical,monitorWarning,monitorDebug,monitorUnknown:
+	case monitorOk, monitorCritical, monitorWarning, monitorDebug, monitorUnknown:
 		// These are valid
 	default:
 		panic("unknown monitoring state")
@@ -38,9 +38,31 @@ func monitor(state monitoringResult, message string) {
 	call = strings.Replace(call, "%(state)", string(state), -1)
 	call = strings.Replace(call, "%(message)", message, -1)
 	// do argument interpolation
-	cmd := exec.Command("/bin/sh", "-c", call)
+	cmd := commander.Command("/bin/sh", "-c", call)
 	err := cmd.Run()
 	if err != nil {
 		log.Fatalln("FATAL: Monitoring script failed, hope your monitoring system detects dead clients")
 	}
+}
+
+// infrastructure for dependency injection for os.exec Command and run
+type Executor interface {
+	Run() error
+}
+
+type Commander interface {
+	Command(name string, args ...string) Executor
+}
+
+var commander = execCommander{}
+
+// default implementations
+type execExecutor struct {
+	*exec.Cmd
+}
+
+type execCommander struct{}
+
+func (e execCommander) Command(name string, args ...string) Executor {
+	return execExecutor{exec.Command(name, args...)}
 }
