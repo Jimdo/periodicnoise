c7abeb6d4affc9458dedd21df21c4b3402c251df
Ingo Oeser <ingo@jimdo.com>
2013-11-15 14:15:17 +0100

    distinguish between transient and permanent errors
    In order to only retry on temporary problems, but fail on permanent
    problems, let's build some logic to distinguish these.
    
    We have 3 clear cases
    
     1. We are beyond our time budget -> fail immediately
     2. The remote is not reachable -> retry, if we could ever connect,
        fail otherwise
     3. The net package classifies this as Temporary condition or Timeout ->
        retry
    
    All other cases are considered permanent failures.

--- cmd/pn/log.go
+++ cmd/pn/log.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"os"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/Jimdo/periodicnoise/syslog"
 )
@@ -24,20 +27,53 @@ func getLogger(useSyslog bool) (logger io.Writer, err error) {
 	return &LineWriter{w: logger}, err
 }
 
+func canContinue(expire time.Time, had_progress bool, err error) bool {
+	if time.Now().After(expire) {
+		return false
+	}
+
+	if netErr, ok := err.(net.Error); ok && (netErr.Temporary() || netErr.Timeout()) {
+		return true
+	} else if opErr, ok := err.(*net.OpError); ok && had_progress {
+		if errno, ok := opErr.Err.(syscall.Errno); ok {
+			// These are only temporary errors, if we could ever connect.
+			// If we could never connect, we cannot decide, whether this is a temporary failure,
+			// or the address we connect to is simply wrong.
+			switch errno {
+			case syscall.ECONNREFUSED:
+				fallthrough
+			case syscall.ENETUNREACH:
+				fallthrough
+			case syscall.EHOSTUNREACH:
+				return true
+			}
+		}
+	}
+	return false
+}
+
 // pipe r to logger in the background
 func logStream(r io.Reader, logger io.Writer, wg *sync.WaitGroup) {
 	wg.Add(1)
 
 	go func() {
+		expire := time.Now().Add(opts.Timeout + 500*time.Millisecond)
+
+		var written int64
+
 		// Retry writing to logstream until it can be fully written.
 		// If io.Copy returns nil, everything has been copied
 		// successfully and this routine can be stopped.
 		for {
-			if _, err := io.Copy(logger, r); err == nil {
+			n, err := io.Copy(logger, r)
+			written += n
+			if err == nil {
 				break
-			} else {
-				fmt.Fprintln(os.Stderr, "pn log error:", err)
+			} else if !canContinue(expire, written > 0, err) {
+				fmt.Fprintln(os.Stderr, "permanent pn log error:", err)
+				break
 			}
+			fmt.Fprintln(os.Stderr, "transient pn log error:", err)
 		}
 		wg.Done()
 	}()
