e5cf2e3829afc2ba3ccb28f55fec056d9ba67cc6
Ingo Oeser <ingo@jimdo.com>
2013-11-21 18:31:50 +0100

    explain and check --grace-time
    In order to better explain what --grace-time vs. --timeout means,
    change option description.
    
    Also check for sane grace time value.

--- cmd/pn/flags.go
+++ cmd/pn/flags.go
@@ -9,7 +9,7 @@ import (
 
 var opts struct {
 	MaxDelay         time.Duration `short:"d" long:"max-start-delay" description:"optional maximum execution start delay for command, e.g. 45s, 2m, 1h30m"`
-	Timeout          time.Duration `short:"t" long:"timeout" default:"1m" description:"set execution timeout for command, e.g. 45s, 2m, 1h30m"`
+	Timeout          time.Duration `short:"t" long:"timeout" default:"1m" description:"set hard execution timeout for command, e.g. 45s, 2m, 1h30m"`
 	UseSyslog        bool          `short:"s" long:"use-syslog" description:"log via syslog instead of stderr"`
 	WrapNagiosPlugin bool          `short:"n" long:"wrap-nagios-plugin" description:"wrap nagios plugin (pass on return codes, pass first 8KiB of stdout as message)"`
 	NoPipeStderr     bool          `long:"no-stream-stderr" description:"do not stream stderr to log"`
@@ -17,7 +17,7 @@ var opts struct {
 	MonitoringEvent  string        `short:"E" long:"monitor-event" description:"monitoring event (defaults to check_foo for /path/check_foo.sh)"`
 	KillRunning      bool          `short:"k" long:"kill-running" description:"kill already running instance of command"`
 	NoMonitoring     bool          `long:"no-monitoring" description:"wrap command without sending monitoring events"`
-	GraceTime        time.Duration `long:"grace-time" default:"10s" description:"execution timeout for command before sending SIGTERM, e.g. 45s, 2m, 1h30m"`
+	GraceTime        time.Duration `long:"grace-time" default:"10s" description:"time left until TIMEOUT, before sending SIGTERM to command, e.g. 45s, 2m, 1h30m"`
 }
 
 func parseFlags() []string {
@@ -42,6 +42,10 @@ func parseFlags() []string {
 
 	if opts.MaxDelay >= opts.Timeout {
 		log.Fatal("FATAL: max delay >= timeout, no time left for actual command execution")
+	}
+
+	if opts.GraceTime >= opts.Timeout {
+		log.Fatal("FATAL: grace time >= timeout, no time left for actual command execution")
 	}
 
 	return args
